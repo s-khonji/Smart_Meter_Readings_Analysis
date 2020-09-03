980ef293ec3dbd69ae6e9d21f12fef8c5e1ef722
Sam Khonji <58490547+s-khonji@users.noreply.github.com>
2020-09-03 15:17:48 +0100

    Add files via upload

---
+++ estimate_usage_cost.py
@@ -0,0 +1,59 @@
+
+import pandas as pd
+import numpy as np
+
+"""Script to estimate monthly electricity usage costs for each household (meter_id), for 
+meter readings contained in 'ee_coding_challenge_dataset.csv'.
+
+The output of the script is a printed table showing the estimated costs.
+
+This script requires `pandas` and 'numpy' to be installed within the Python
+environment you are running this script in.
+
+"""
+
+def main():
+
+    #read csv file
+    data = pd.read_csv('ee_coding_challenge_dataset.csv')
+    df = pd.DataFrame(data)
+
+    #create DatTimeIndex
+    df['DateTime'] = pd.to_datetime(df['DateTime'], dayfirst = True)
+    df.set_index(pd.DatetimeIndex(df['DateTime']), inplace = True) 
+    df.drop(columns = ['DateTime'], inplace = True)
+
+    # clean data by removing consumption reading errors
+    df = df[(df['consumption']>0) & (df['consumption']<3) & (df['consumption']!=0)]
+
+    # Add 'Month' and 'Year' columns
+    df['Month'] = df.index.month
+    df['Year'] = df.index.strftime("%Y")
+
+    grouped_df = pd.DataFrame(df.groupby(['meter_id', 'Month', 'Year'])[['consumption']].sum())
+    grouped_df.reset_index(inplace = True)
+
+    # Estimate usage cost with flat rate
+    grouped_df['Electricity cost (£) on current flat rate tariff'] = grouped_df['consumption']*0.15
+    
+    # Convert month number to name
+    grouped_df['Month'] = grouped_df['Month'].apply(month_name)
+
+    # Drop consumption column
+    grouped_df.drop(columns = ['consumption'], inplace = True)
+
+    # Print desired table showing estimated usage costs based on flat rate
+    print(grouped_df)
+
+def month_name(num):
+    
+    """Converts month number to name. 
+    This allows the use of vectorized operations when combined with .apply() function"""
+    
+    month_dict = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June'\
+                  , 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
+    
+    return month_dict[num]
+
+if __name__ == "__main__":
+    main()
---
+++ filter_readings.py
@@ -0,0 +1,121 @@
+
+"""Script to filter meter readings contained in 'ee_coding_challenge_dataset.csv'
+
+This script allows the user to meter readings by meter_id, month and year based on user inputs into the console.
+
+Example of inputting months for February (2nd month), April (4th month) and May (5th month) would be: 2,4,5
+
+Example of inputting the years 2012 and 2013 would be: 2012, 2013
+
+This script requires that `pandas` and 'numpy' to be installed within the Python
+environment you are running this script in.
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * clean_consumption_data
+    * filter_meter_data
+    * Filter_month
+    * filter_year
+    * main - the main function of the script
+"""
+
+import pandas as pd
+import numpy as np
+
+def main():
+    
+    data = pd.read_csv('ee_coding_challenge_dataset.csv')
+    df = pd.DataFrame(data)
+    
+    df = clean_consumption_data(df)
+
+    df['DateTime'] = pd.to_datetime(df['DateTime'], dayfirst=True)
+    df.set_index(pd.DatetimeIndex(df['DateTime']), inplace = True) 
+    df.drop(columns = ['DateTime'], inplace = True)
+
+    user_input = ''
+    
+    user_input = input('Would you like to filter by meter_id? [y/n]')
+    
+    while user_input not in {'Y', 'y', 'N', 'n'}:
+        user_input = input('Please select [y/n].')
+    
+    if user_input in {'y', 'Y'}:
+        user_input = input('Please enter the meter_id.')
+        df_filter = filter_meter_id(df, user_input)
+        
+    elif user_input in {'n', 'N'}:
+        df_filter = df
+        pass
+     
+
+    user_input = input('Would you like to filter by month [y/n]?')
+
+    while user_input not in {'Y', 'y', 'N', 'n'}:
+        user_input = input('Please select [y/n].')
+    
+    if user_input in {'y', 'Y'}:
+        user_input = input('Please enter the numbers of the months separated by commas.')
+        df_filter = filter_month(df_filter, user_input)
+    
+    elif user_input in {'n', 'N'}:
+        pass
+
+    user_input = input('Would you like to filter by year? [y/n]')
+
+    while user_input not in {'Y', 'y', 'N', 'n'}:
+        user_input = input('Please select [y/n].')
+    
+    if user_input in {'y', 'Y'}:
+        user_input = input('Please enter the numbers of the years separated by commas (data only exists for 2013).')
+        df_filter = filter_year(df_filter, user_input)
+    
+    elif user_input in {'n', 'N'}:
+        pass
+    
+    print(df_filter)
+
+def clean_consumption_data(df):
+    
+    """Takes in a dataframe containg meter readings and removes all rows in which:
+        
+        * consumption reading is below 0 (not possible)
+        * consumption reading is above 3 (too high)
+        * consumption reading is equal to zero (generally not possible)
+    """
+    
+    df = df[(df['consumption']>0) & (df['consumption']<3) & (df['consumption']!=0)]
+    
+    return df
+
+def filter_meter_id(df, m_id):
+    
+    """Takes in a dataframe containg meter readings and removes all rows that do not have the 
+    meter_id specified
+    
+    """
+    return df[df['meter_id'] == str(m_id)]
+
+def filter_month(df, months):
+    
+    
+    """Takes in a series of comma-separated numbers representing months (i.e. January = 1) and filters the dataframe 
+    based on the numbers provided.
+    
+    """
+    
+    return df[df.index.month.isin([int(month) for month in months.split(',')])]
+
+def filter_year(df, years):
+    
+    
+    """Takes in a series of comma-separated numbers representing years (i.e. 2014, 2015) and filters the dataframe 
+    based on the numbers provided.
+    """
+    
+    return df[df.index.year.isin([int(year) for year in years.split(',')])]
+
+
+if __name__ == "__main__":
+    main()
